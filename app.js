0618ab2cdb33950be45b88411fbad5a0d08e94d1
Rahim <k.rahim001@gmail.com>
2023-08-19 11:00:40 +0530

    RJ(express): install express framework to our node js project

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log("Rahim");
+// console.log("Rahim");
 // npm init
 // npm i nodemon --save-dev
+// npm i --seve express
+
+const http = require('http');
+const server = http.createServer();
+console.log("server starting");
+server.listen(3000);
