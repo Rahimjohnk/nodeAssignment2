405171887893e6f88be01b278e69ce060054f9a7
Rahim <k.rahim001@gmail.com>
2023-08-19 11:21:51 +0530

    RJ(routes): handling routes

--- app.js
+++ app.js
@@ -3,7 +3,29 @@
 // npm i nodemon --save-dev
 // npm i --seve express
 
-const http = require('http');
-const server = http.createServer();
+// const http = require('http');
+const express = require('express');
+const app = express();
+app.use('/users',(req, res, next)=>{
+    // console.log("server starting 0");
+    // next();
+    res.send(`
+    <h1>users</h1>
+    `);
+});
+app.use('/', (req, res, next)=>{
+    // console.log("server starting 0");
+    // next();
+    res.send(`
+    <h1>Rahimjohn</h1>
+    `);
+});
+// app.use((req, res, next)=>{
+//     console.log("server starting 1");
+// });
 console.log("server starting");
-server.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
+// or
+app.listen(3000);
